96c1e1cf1547ef8316e270f1903aa1fbb708634c
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-29 04:09:39 -0500

    feat: :rocket: Introducción al PHP
    Se inicio un repositorio donde se va a poner en practica algunos conocimiento de php ademas de ir documentando con ayuda del ManualPHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- Estructura Basica de un Script
+
+Carpetas:
+-CSS
+-img
+-js
+-scrips
+-uploads
+
+Archivo:
+-index.php
+
+
+-----------------¿Qué es PHP?-----------------
+Es un lenguaje de código abierto muy popular especialmente adecuado para el desarrollo web que corre del lado del servidor y que puede ser incrustado en HTML.  -->
