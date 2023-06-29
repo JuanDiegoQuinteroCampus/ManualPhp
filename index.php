df3c79b7c45499cbc53a9a57d644db231d4a3917
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-29 04:31:51 -0500

    feat: :sparkles: Variables y constantes
    Se exolico que son las variables y las constantes, ademas de que se le dio el ejemplo

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
-// Funciones de salida en PHP
-// Las funciones de salida son aquellas que permiten mostrar al usuario cuando se finaliza la ejecucion de un proceso determinado, algunas de estas funciones son:
+// Variables y constantes
 
+// Las variables en PHP se definen usando el símbolo $ seguido del nombre de la variable. La asignación
+// de un valor a la variable se realiza utilizando el operador =.
 
-// -echo(): Esta es la función más común para imprimir en PHP. Se utiliza para mostrar una o
-// varias cadenas de texto en la salida del servidor web. La sintaxis básica es la siguiente:
+// Declaracion numerica
+$num = 23;
+// Declaracion texto
+$texto = "Hola mundo";
+// /Declaracion booleano
+$bool =true;
+echo var_dump($texto); //se está imprimiendo el tipo de dato y el contenido de la variable haciendo uso de la función especial var_dump.
 
-echo "Kiubo mano como me le va?";
+//Declaracion de constantes(no cambian)
+//Numerica
+define("PI",3.2323);
+//Texto
+define("TEXTO","Kiubo mano");
+//Boolenao
+define("BOOL",true);
 
-// -print(): Esta función es similar a echo(), pero solo puede imprimir una cadena de texto a la vez.
-// La sintaxis básica es la siguiente:
-
-$texto = "Chavales";
-printf("Hola%s",$texto); //el marcador %s, indica que se trata de una cadena de texto
-
-// -sprintf(): Esta función es similar a printf(), pero en lugar de imprimir la cadena formateada en
-// la salida estándar, devuelve la cadena formateada como resultado. La sintaxis básica es la
-// siguiente:
-
-$texto = "Chavales";
-$mensaje = sprintf("Hola%s",$texto);
-echo $mensaje;
 
 ?>
