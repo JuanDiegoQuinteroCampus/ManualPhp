673e9a3d34df190ee963a05a8ab9411ca3365203
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-29 04:22:27 -0500

    feat: :sparkles: Funciones de salida en PHP
    Se explica que son las funciones de salida y cuales son algunas de estas con su respectivo ejemplo

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<!-- Estructura Basica de un Script
-
-Carpetas:
--CSS
--img
--js
--scrips
--uploads
-
-Archivo:
--index.php
+<?php
+// Funciones de salida en PHP
+// Las funciones de salida son aquellas que permiten mostrar al usuario cuando se finaliza la ejecucion de un proceso determinado, algunas de estas funciones son:
 
 
------------------¿Qué es PHP?-----------------
-Es un lenguaje de código abierto muy popular especialmente adecuado para el desarrollo web que corre del lado del servidor y que puede ser incrustado en HTML.  -->
+// -echo(): Esta es la función más común para imprimir en PHP. Se utiliza para mostrar una o
+// varias cadenas de texto en la salida del servidor web. La sintaxis básica es la siguiente:
+
+echo "Kiubo mano como me le va?";
+
+// -print(): Esta función es similar a echo(), pero solo puede imprimir una cadena de texto a la vez.
+// La sintaxis básica es la siguiente:
+
+$texto = "Chavales";
+printf("Hola%s",$texto); //el marcador %s, indica que se trata de una cadena de texto
+
+// -sprintf(): Esta función es similar a printf(), pero en lugar de imprimir la cadena formateada en
+// la salida estándar, devuelve la cadena formateada como resultado. La sintaxis básica es la
+// siguiente:
+
+$texto = "Chavales";
+$mensaje = sprintf("Hola%s",$texto);
+echo $mensaje;
+
+?>
